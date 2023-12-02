a71d0f1d3f0ac9be6772477cd39337a08c9b158b
Boris Kloris <throwaway.nirrti@gmail.com>
2023-12-02 15:21:02 +0530

    clean day 2 solution

--- 2023/2.hs
+++ 2023/2.hs
@@ -3,27 +3,15 @@
 import System.IO (readFile)
 import Data.Maybe (catMaybes)
 import Data.Text as T (splitOn, pack, Text, drop, unpack, strip)
-import Data.Text.Read (decimal)
 
 main :: IO ()
 main = do
     contents <- readFile "input_2.txt"
-    processCases sumGameIds $ contents
-    processCases sumPowerSets $ contents
+    processCases (catMaybes . map playable) $ contents
+    processCases (map power) $ contents
 
-processCases :: Show b => ([String] -> b) -> String -> IO ()
-processCases f = print . f . lines
-
-sumGameIds :: [String] -> Int
-sumGameIds = sum . catMaybes . map (playable . toGame)
-
-sumPowerSets :: [String] -> Int
-sumPowerSets = sum . map (power . toGame)
-
-power :: Game -> Int
-power (Game _ draws) = r * g * b
-    where
-        (r, g, b) = minimalSet draws
+processCases :: (Show b, Foldable t, Num b) => ([Game] -> t b) -> String -> IO ()
+processCases f = print . sum . f . map toGame . lines
 
 data Game  = Game Int Draws
 type Draws = [Draw]
@@ -32,16 +20,20 @@ type Draw  = (Int, Int, Int)
 playable :: Game -> Maybe Int
 playable (Game id draws) = if checkDraws draws then Just id else Nothing
 
+power :: Game -> Int
+power (Game _ draws) = r * g * b
+    where
+        (r, g, b) = minimalSet draws
+
 checkDraws :: Draws -> Bool
 checkDraws draws = r <= 12 && g <= 13 && b <= 14
     where
         (r, g, b) = minimalSet draws
 
-maxDraws :: Draw -> Draw -> Draw
-maxDraws (r, g, b) (r1, g1, b1) = (max r r1, max g g1, max b b1)
-
 minimalSet :: Draws -> Draw
 minimalSet = foldr maxDraws (0, 0, 0)
+    where
+        maxDraws (r, g, b) (r1, g1, b1) = (max r r1, max g g1, max b b1)
 
 toGame :: String -> Game
 toGame line = Game id draws
