dd4ffff3451d1d0ab66d06c018f27426346b3257
Boris Kloris <throwaway.nirrti@gmail.com>
2023-12-03 20:58:43 +0530

    add day 3 solutions

---
+++ 2023/3.hs
@@ -0,0 +1,62 @@
+import Data.List (tails, foldl')
+import Data.Char (isDigit, digitToInt)
+
+main :: IO ()
+main = do
+    contents <- readFile "input_3.txt"
+    processCases (map (getSums . getContext)) id $ contents
+    processCases (map getPowers) (map expandNumbers) $ contents
+
+processCases :: ([(a, a, a)] -> [Int]) -> ([String] -> [a]) -> String -> IO ()
+processCases f g = print . sum . f . matchUp . g . padLines . map pad . lines
+
+pad :: String -> String
+pad str = "." ++ str ++ "."
+
+padLines :: [String] -> [String]
+padLines strs = [padLine] ++ strs ++ [padLine]
+    where
+        padLine = (take . length . head $ strs) . repeat $ '.'
+
+matchUp :: [a] -> [(a, a, a)]
+matchUp ls = zip3 ls (tail ls) (tail . tail $ ls)
+
+getContext :: (String, String, String) -> [(Char, Bool)]
+getContext (prev, curr, next) = zip (tail curr) summation
+    where
+        getRowContext = map (take 3) . tails
+        rowContext = zipWith (++) (getRowContext prev) (getRowContext next)
+        neighbours = zipWith (\a b -> [a, b]) curr (tail . tail $ curr)
+        context = zipWith (++) rowContext neighbours
+        isSymbol x = not (isDigit x) && (x /= '.')
+        summation = (map (any isSymbol) context) ++ [False]
+
+getSums :: [(Char, Bool)] -> Int
+getSums = getThird . foldl' folder (False, 0, 0)
+    where
+        getThird (_, _, x) = x
+
+folder :: (Bool, Int, Int) -> (Char, Bool) -> (Bool, Int, Int)
+folder (soFar, curr, total) (c, t)
+    | isDigit c = (soFar || t, 10 * curr + digitToInt c, total)
+    | otherwise = (False, 0, total + if soFar then curr else 0)
+
+expandNumbers :: String -> [String]
+expandNumbers = concatMap repeatLength . snd . foldr readNumbers ("", [])
+    where
+        repeatLength x = (take . length $ x) . repeat $ x
+
+readNumbers :: Char -> (String, [String]) -> (String, [String])
+readNumbers c (soFar, ls)
+    | isDigit c = (c:soFar, ls)
+    | otherwise = ("", [[c]] ++ (if soFar == "" then [] else [soFar]) ++ ls)
+
+getPowers :: ([String], [String], [String]) -> Int
+getPowers (prev, curr, next) = sum (zipWith (\a b -> if (a == "*" && length b == 2) then product b else 0) (tail curr) nums)
+    where
+        isNum x = isDigit . head $ x
+        diag ls = zipWith3 (\a b c -> if isNum b then [b] else filter isNum [a, c]) 
+                    ls (tail ls) (tail . tail $ ls)
+        nei = zipWith (\a b -> filter isNum [a, b]) curr (tail . tail $ curr)
+        context = zipWith3 (\a b c -> a ++ b ++ c) (diag prev) nei (diag next)
+        nums = (map . map) (read :: String -> Int) context
