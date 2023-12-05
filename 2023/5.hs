c66295ca07fe2dc6ca1d822c3795c87b78a0fddd
Boris Kloris <throwaway.nirrti@gmail.com>
2023-12-05 21:22:11 +0530

    refactor: rename functions and types

--- 2023/5.hs
+++ 2023/5.hs
@@ -10,25 +10,28 @@ main = do
 
     let seeds = map (read :: String -> Int) . words . drop 6 . unpack . strip . head $ sections
     let maps  = map readMaps . tail $ sections
-    print . minimum . map (($ maps) . foldl' mapIt) $ seeds
+    print . minimum . map (($ maps) . foldl' mapOne) $ seeds
 
     let seedRanges = liftM2 zip (indices even) (indices odd) $ seeds
     print . minimum . map fst . foldl' (\s m -> concatMap (mapRange m) s) seedRanges $ maps
 
-readMaps :: Text -> [[Int]]
+type Map = [[Int]]
+type Range = (Int, Int)
+
+readMaps :: Text -> Map
 readMaps = sortBy (\(_:a:_) (_:b:_) -> compare a b) . (map . map) (read :: String -> Int) . map words . tail . lines . unpack
 
-mapIt :: Int -> [[Int]] -> Int
-mapIt a [] = a
-mapIt a ([dest, origin, range]:ms)
+mapOne :: Int -> Map -> Int
+mapOne a [] = a
+mapOne a ([dest, origin, range]:ms)
     | a < origin = a
     | a >= origin && a < origin + range = dest + (a - origin)
-    | otherwise = mapIt a ms
+    | otherwise = mapOne a ms
 
 indices :: (Int -> Bool) -> [a] -> [a]
 indices f = map snd . filter (f . fst). zip [0..]
 
-mapRange :: [[Int]] -> (Int, Int) -> [(Int, Int)]
+mapRange :: Map -> Range -> [Range]
 mapRange [] (start, range) = if range <= 0 then [] else [(start, range)] 
 mapRange m@([dest, origin, span]:ls) r@(start, range)
     | range <= 0       = []
