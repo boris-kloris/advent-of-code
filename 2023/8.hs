cdccd0404ff7de5754f0f39c81225fb38dcb6eb6
Boris Kloris <throwaway.nirrti@gmail.com>
2023-12-08 19:17:02 +0530

    cleanup solution

--- 2023/8.hs
+++ 2023/8.hs
@@ -1,5 +1,6 @@
 import Data.Map (Map, fromList, lookup)
 import Data.Maybe (fromMaybe)
+import Control.Monad (liftM2)
 
 main :: IO ()
 main = do
@@ -10,17 +11,23 @@ main = do
     let driver = follow . fromList $ nodes
     print . step . getSteps driver (=="ZZZ") $ State "AAA" 0 instructions
 
-    let startNodes = filter ((=='A') . last) . map fst $ nodes
-    let endStates = map (getSteps driver ((=='Z') . last)) $ zipWith3 State startNodes (repeat 0) (repeat instructions)
-    let periods   = map (period driver ((=='Z') . last)) endStates
-    let offsets   = zipWith mod (map step endStates) (map step periods)
+    let endsWith c node = last node == c
+    let startNodes = filter (endsWith 'A') . map fst $ nodes
+    let startStates = zipWith3 State startNodes (repeat 0) (repeat instructions)
+    let endStates = map (getSteps driver (endsWith 'Z')) startStates
+    let periodicStates = map (period driver (endsWith 'Z')) endStates
+
+    let unpack = liftM2 (,) (map node) (map step)
+    let (endNodes, endSteps) = unpack endStates
+    let (periodNodes, periodSteps) = unpack periodicStates
+    let offsets = zipWith mod endSteps periodSteps
 
     -- THIS IS A HACK!!!
     putStrLn "Are all the offsets zero, and the 'Z' nodes are the only ones in the cycle?"
-    putStrLn $ if all (==0) offsets && map node periods == map node endStates
+    putStrLn $ if all (==0) offsets && endNodes == periodNodes
         then "YES!!! The following answer is correct!"
         else "No, The following answer can't be trusted. :("
-    print . foldr lcm 1 . map step $ periods
+    print . foldr lcm 1 $ periodSteps
 
 type Node = String
 type Step = Int
