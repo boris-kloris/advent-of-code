edb485b6d9297132b05d396aa0777868ed981ac5
Boris Kloris <throwaway.nirrti@gmail.com>
2023-12-08 18:24:14 +0530

    add day 8 solution

---
+++ 2023/8.hs
@@ -0,0 +1,49 @@
+import Data.Map (Map, fromList, lookup)
+import Data.Maybe (fromMaybe)
+
+main :: IO ()
+main = do
+    contents <- lines <$> readFile "input_8.txt"
+
+    let instructions = concat . repeat . head $ contents
+    let nodes = map readLink . tail . tail $ contents
+    let driver = follow . fromList $ nodes
+    print . step . getSteps driver (=="ZZZ") $ State "AAA" 0 instructions
+
+    let startNodes = filter ((=='A') . last) . map fst $ nodes
+    let endStates = map (getSteps driver ((=='Z') . last)) $ zipWith3 State startNodes (repeat 0) (repeat instructions)
+    let periods   = map (period driver ((=='Z') . last)) endStates
+    let offsets   = zipWith mod (map step endStates) periods
+
+    -- THIS IS A HACK!!!
+    putStrLn "Are all the offsets zero?"
+    putStrLn $ if all (==0) offsets
+        then "YES!!! The following answer is correct!"
+        else "No, The following answer can't be trusted. :("
+    print . foldr lcm 1 $ periods
+
+type Node = String
+type Step = Integer
+type Instructions = String
+type Turn = Char
+
+data State = State { node :: Node, step :: Step, currentInstructions :: Instructions }
+
+readLink :: String -> (Node, (Node, Node))
+readLink str = (node 0 str, (node 7 str, node 12 str))
+    where
+        node pos = take 3 . drop pos
+
+follow :: Map Node (Node, Node) -> Turn -> Node -> Node
+follow map turn node = if turn == 'L' then left else right
+    where
+        (left, right) = fromMaybe ("", "") . Data.Map.lookup node $ map
+
+getSteps :: (Turn -> Node -> Node) -> (Node -> Bool) -> State -> State
+getSteps driver stopPredicate (State node num instructions@(turn:turns))
+    | stopPredicate node = State node num instructions
+    | otherwise = getSteps driver stopPredicate $ State (driver turn node) (num+1) turns
+
+period :: (Turn -> Node -> Node) -> (Node -> Bool) -> State -> Integer
+period driver stopPredicate (State node num (turn:turns)) =
+    step (getSteps driver stopPredicate (State (driver turn node) (num+1) turns)) - num
