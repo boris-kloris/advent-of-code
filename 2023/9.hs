c517eec343636619026398c678f3d73f098f679e
Boris Kloris <throwaway.nirrti@gmail.com>
2023-12-09 14:13:36 +0530

    refactor out extrapolation functions

--- 2023/9.hs
+++ 2023/9.hs
@@ -1,8 +1,14 @@
 main :: IO ()
 main = do
-    sequences <- (map . map) (read :: String -> Int) . map words . lines <$> readFile "input_9.txt"
-    print . sum . map (sum . map last . diffPyramid) $ sequences
-    print . sum . map (foldr (-) 0 . map head . diffPyramid) $ sequences
+    sequences <- map ( map (read :: String -> Int) . words ) . lines <$> readFile "input_9.txt"
+    print . sum . map extrapolateForwards $ sequences
+    print . sum . map extrapolateBackwards $ sequences
+
+extrapolateForwards :: [Int] -> Int
+extrapolateForwards = sum . map last . diffPyramid
+
+extrapolateBackwards :: [Int] -> Int
+extrapolateBackwards = foldr (-) 0 . map head . diffPyramid
 
 diffPyramid :: [Int] -> [[Int]]
 diffPyramid = (takeWhile . any) (/= 0) . iterate diffSequence
